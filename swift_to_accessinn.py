a82f4a3d80a640814a264edf41cffa93c15fc6c5
Tianyu Zhang <skyu0221@users.noreply.github.com>
2019-09-12 12:08:04 -0600

    Small fix

--- swift_to_accessinn.py
+++ swift_to_accessinn.py
@@ -25,7 +25,7 @@ language_abbrev_map = {"en": "english", "fr": "french"}
 def ftp_upload():
     # Generate a temp folder, make sure the folder not in use
     while True:
-        temp_folder = "temp_" + str(uuid4())
+        temp_folder = "~/temp_" + str(uuid4())
         if not os.path.isdir(temp_folder):
             break
     subprocess.run(["mkdir", temp_folder])
@@ -79,32 +79,44 @@ def ftp_upload():
     for data in cursor:
         current_count += 1
         noid, news_abbrev, year, month, day, language = data
-        counter = 0
+        counter = 1
 
-        # For now, always put into 'separate' folder, but need to put into different language folder
-        ftps.cwd("%s/separate" % language_abbrev_map[language])
-        while True:
-            counter += 1
+        try:
+            # For now, always put into 'separate' folder, but need to put into different language folder
+            ftps.cwd("%s/separate" % language_abbrev_map[language])
+            # while True:
+            #     upload_folder_name = "%s-%d%02d%02d%02d" % (news_abbrev, year, month, day, counter)
+            #     if upload_folder_name not in processed_news[language_abbrev_map[language]]:
+            #         break
+            #     counter += 1
+
             upload_folder_name = "%s-%d%02d%02d%02d" % (news_abbrev, year, month, day, counter)
-            if upload_folder_name not in processed_news[language_abbrev_map[language]]:
-                break
+            if upload_folder_name in processed_news[language_abbrev_map[language]]:
+                ftps.cwd("../..")
+                print("%s already exist on server. NOID: %s" % (upload_folder_name, noid))
+                continue
 
-        # Generate folders on FTP server
-        # Download from OpenStack Swift, upload to FTP server
-        ftps.mkd(upload_folder_name)
-        for target_folder in ("mets", "alto"):
-            print("Transmitting %s/%s, work done: %d out of %d %.2f%%" % (upload_folder_name, target_folder,
-                                                                          current_count, total_newspaper,
-                                                                          100 * current_count / total_newspaper))
-            ftps.mkd("%s/%s" % (upload_folder_name, target_folder))
-            subprocess.run(["swift", "download", "newspapers", "%s/%s/1.tar" % (noid, target_folder),
-                            "-o", "%s/1.tar" % temp_folder])
-            with open("%s/1.tar" % temp_folder, "rb") as transmit_file:
-                ftps.storbinary("STOR %s/%s/1.tar" % (upload_folder_name, target_folder), transmit_file)
-            subprocess.run(["rm", "-f", "%s/1.tar" % temp_folder])
+            # Generate folders on FTP server
+            # Download from OpenStack Swift, upload to FTP server
+            ftps.mkd(upload_folder_name)
+            for target_folder in ("mets", "alto"):
+                print("Transmitting %s/%s, work done: %d out of %d %.2f%%" % (upload_folder_name, target_folder,
+                                                                              current_count, total_newspaper,
+                                                                              100 * current_count / total_newspaper))
+                ftps.mkd("%s/%s" % (upload_folder_name, target_folder))
+                subprocess.run(["swift", "download", "newspapers", "%s/%s/1.tar" % (noid, target_folder),
+                                "-o", "%s/1.tar" % temp_folder])
+                with open("%s/1.tar" % temp_folder, "rb") as transmit_file:
+                    ftps.storbinary("STOR %s/%s/1.tar" % (upload_folder_name, target_folder), transmit_file)
+                subprocess.run(["rm", "-f", "%s/1.tar" % temp_folder])
 
-        # Get back to the production folder to reselect the language
-        ftps.cwd("../..")
+            # Get back to the production folder to reselect the language
+            ftps.cwd("../..")
+        except:
+            ftps.cwd("~/uploads/production")
+            with open("%s_errorlog.txt" % temp_folder, 'w') as error_log:
+                print("Error occurs when transmitting %s." % noid)
+                error_log.write("Error occurs when transmitting %s.\n" % noid)
 
     cnx.close()
     ftps.close()
